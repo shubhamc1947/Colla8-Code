4be6186ae01d60518bdec7dce2fbfb1ad638f5fa
shubham-covrzy <shubham@covrzy.com>
2025-05-20 20:36:23 +0530

    added guest login log

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,7 @@ export const login = async (credentials) => {
   // console.log(API_BASE_URL);
   try {
     const response = await axios.post(`${API_BASE_URL}/api/auth/login`, credentials, { withCredentials: true });
-    console.log(response.data);
+    // console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error during login:', error);
