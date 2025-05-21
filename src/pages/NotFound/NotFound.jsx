f91acade4acd6b13a922034fd5a5e497fc5f72f4
shubham-covrzy <shubham@covrzy.com>
2025-05-21 09:41:25 +0530

    added not fouond page and created a small motionvarient page for refactor motion code

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,33 @@
-import './notfound.scss'
+import { Link } from "react-router-dom"
+import "./notfound.scss"
+import { motion } from "framer-motion"
+import { fadeDownVariant } from "../../utils/motionVarients"
+
 const NotFound = () => {
   return (
-    <div className='notfound'>NotFound</div>
+    <motion.div className="notfound" variants={fadeDownVariant} initial="initial" animate="animate">
+      <div className="notfound-content">
+        <div className="error-code">404</div>
+        <div className="code-snippet">
+          <pre>
+            <code>
+              <span className="keyword">try</span> {"{"}
+              <br />
+              {"  "}findPage(<span className="string">&quot;{window.location.pathname}&quot;</span>);
+              <br />
+              {"}"} <span className="keyword">catch</span> (error) {"{"}
+              <br />
+              {"  "}console.error(<span className="string">&quot;404: Page not found&quot;</span>);
+              <br />
+              {"}"}
+            </code>
+          </pre>
+        </div>
+        <Link to="/" className="home-button">
+          Back to Home
+        </Link>
+      </div>
+    </motion.div>
   )
 }
 
---
+++ src/utils/motionVarients.js
@@ -0,0 +1,15 @@
+export const fadeDownVariant = {
+    initial: {
+      y: -100,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        delay: 0.3,
+      },
+    },
+  };
+  
