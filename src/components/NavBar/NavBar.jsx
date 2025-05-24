1d00fd40c72d4b061a004157c39317d129e90f9c
shubham-covrzy <shubham@covrzy.com>
2025-05-24 18:45:34 +0530

    added notif for server side latency

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 // src/components/NavBar/NavBar.js
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import "./NavBar.scss";
 //framer motion
 import { motion } from "framer-motion"
+import { toast } from "react-toastify";
 
 const NavBar = () => {
   const { authState, logoutHandler, loading } = useContext(AuthContext);
@@ -29,6 +30,20 @@ const NavBar = () => {
       }
     }
   }
+  useEffect(() => {
+      let timeout;
+      if (loading) {
+        timeout = setTimeout(() => {
+          toast.info("⏳ The server is on a free tier, so it might take a few seconds. Trust me, the app works!");
+        }, 2000);
+      }
+      return () => clearTimeout(timeout);
+  }, [loading]);
+  useEffect(() => {
+    if (window.innerWidth < 996) {
+      toast.warn("📱 For the best experience, please use a larger screen.");
+    }
+  }, []);
   return (
     <motion.nav className="navbar" variants={variants} initial="initial" animate="animate">
       <Link to="/"><h1>CollabCode<span>.</span></h1></Link>
